51419cdf3408ff4a246a386fad5199d189c53500
William Owalla <billyowala@gmail.com>
2025-06-28 12:08:20 +0300

    deployment fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,11 @@ export default defineNuxtConfig({
       apiBase: process.env.API_BASE,
     },
   },
+  vite: {
+    server: {
+      allowedHosts: [
+        '2dfb205e-8fcc-4add-8d81-98fc97953b87-00-3l0myr7bahyxs.worf.replit.dev'
+      ]
+    }
+  }
 })
